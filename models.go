53c2c5a0db3a7fae579fcb9a7110b9a26db05b6b
Abdullah el-zohry <91378659+abdullahfarahidy@users.noreply.github.com>
2025-10-31 16:12:14 +0200

    Refactor User model with additional fields

--- models.go
+++ models.go
@@ -1,8 +1,13 @@
 package main
 
+import "time"
+
 type User struct {
-	ID       uint   `gorm:"primaryKey"`
-	Name     string `gorm:"not null"`
-	Email    string `gorm:"unique"`
-	Password string `gorm:"not null"`
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	FirstName string    `gorm:"not null" json:"firstName"`
+	LastName  string    `gorm:"not null" json:"lastName"`
+	Email     string    `gorm:"unique;not null" json:"email"`
+	Password  string    `gorm:"not null" json:"password"`
+	BirthDate string    `json:"birthDate"`
+	CreatedAt time.Time `json:"createdAt"`
 }
